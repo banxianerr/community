86a21bf1b94f643df8b33421d1ccd91f27c12be9
gml <2415829663@qq.com>
2022-07-15 17:09:34 +0800

    add bio col to user1

--- src/main/java/life/majiang/community/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/community/mapper/UserMapper.java
@@ -3,10 +3,14 @@ package life.majiang.community.community.mapper;
 import life.majiang.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user1(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user1 where token = #{token}")
+    User findBytoken(@Param("token") String token);
 }
---
+++ src/main/resources/db/migration/V2__Add_bio_col_to_user1_table.sql
@@ -0,0 +1,5 @@
+alter table USER1
+alter column ID INTEGER auto_increment;
+
+alter table USER1
+    add bio varchar(256);
